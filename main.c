0b472f35b3a600e59bc82790654ede511d0f8a3b
noctyris <noctyris@proton.me>
2025-04-13 11:14:53 +0200

    Refactor SDL initialization and window creation

--- main.c
+++ main.c
@@ -1,37 +1,10 @@
 #include "shared.c"
 
 int main() {
-    // Initialisation de SDL
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        printf("Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
-        return -1;
-    }
-
-    // Création d'une fenêtre
-    SDL_Window *window = SDL_CreateWindow(
-        "Exemple SDL3",          // Titre de la fenêtre
-        800,                     // Largeur
-        600,                     // Hauteur
-        SDL_WINDOW_RESIZABLE     // Drapeaux (fenêtre redimensionnable)
-    );
-
-    if (!window) {
-        printf("Erreur lors de la création de la fenêtre : %s\n", SDL_GetError());
-        SDL_Quit();
-        return -1;
-    }
-
-    SDL_Renderer *renderer = SDL_CreateRenderer(window, "software");
-    if (!renderer) {
-        printf("Erreur lors de la création du renderer : %s\n", SDL_GetError());
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return -1;
-    }
+    initialisation();
 
     // Boucle principale
     int running = 1;
-    SDL_Event event;
 
     while (running) {
         // Gestion des événements
--- shared.c
+++ shared.c
@@ -1,3 +1,37 @@
 #include <SDL3/SDL.h>
 #include <stdio.h>
 
+SDL_Window *window;
+SDL_Renderer *renderer;
+SDL_Event event;
+
+int initialisation() {
+    // Initialisation de SDL
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        printf("Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
+        return -1;
+    }
+
+    // Création d'une fenêtre
+    window = SDL_CreateWindow(
+        "Astro",
+        800, 600,
+        SDL_WINDOW_RESIZABLE
+    );
+
+    if (!window) {
+        printf("Erreur lors de la création de la fenêtre : %s\n", SDL_GetError());
+        SDL_Quit();
+        return -1;
+    }
+
+    renderer = SDL_CreateRenderer(window, "software");
+    if (!renderer) {
+        printf("Erreur lors de la création du renderer : %s\n", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return -1;
+    }
+
+    return 0;
+}
